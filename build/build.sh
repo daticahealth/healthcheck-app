fcb1ea272fe571ee014bfff2a5e7587cec645dad
Nathan Sweet <nathanjsweet@gmail.com>
2018-01-15 16:42:07 -0600

    avoid add

--- build/build.sh
+++ build/build.sh
@@ -7,11 +7,9 @@ fi
 
 rm -f root/bin/healthcheckapp
 GOOS=linux GOARCH=amd64 go build -o root/bin/healthcheckapp ../main.go
-(cd root && fakeroot tar cvf ../root.tar .)
 image=${REGISTRY}/${NAMESPACE}/healthcheckapp
 
 docker build -t ${image}:${TAG} .
 
 #docker push ${image}:${TAG}
 #docker push ${image}:latest
-rm root.tar
